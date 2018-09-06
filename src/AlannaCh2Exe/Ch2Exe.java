e96bb70fa38043872d54ff727a50889a5cdda70a
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-06 14:03:07 -0600

    chapter 2 examples

---
+++ src/AlannaCh2Exe/Ch2Exe.java
@@ -0,0 +1,19 @@
+package AlannaCh2Exe;
+/*
+ Alanna Botscharow
+ 9/18
+ ch2 exe
+
+ */
+
+public class Ch2Exe {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println(10.5+2*3/(45-3.5));
+		
+
+
+	}
+
+}
